b3e223c8288661fb2df55fe7b1e0c7b8327a96e8
devdewa97 <mail.sidewa@gmail.com>
2026-04-19 16:22:35 +0700

    hapus santy

--- digilabs/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import post from './post'
-import category from './category'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, category],
-}
--- sanity.ts
+++
@@ -1,2 +0,0 @@
-export { client, urlFor } from './lib/sanity'
-export { getPost, getPosts } from './lib/sanity'
